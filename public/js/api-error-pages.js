f7e0a457e200debc952a86ad86c7b279e454aa49
Sychedelic-but-cooler <75012033+Sychedelic-but-cooler@users.noreply.github.com>
2026-01-14 01:03:07 +0000

    Fixed an API call error

--- public/js/api-error-pages.js
+++ public/js/api-error-pages.js
@@ -1,13 +1,12 @@
 import api from './api.js';
 
 export async function getErrorPages() {
-  return api._fetch('/api/settings/error-pages', { method: 'GET' });
+  return api.request('/api/settings/error-pages', { method: 'GET' });
 }
 
 export async function saveErrorPage(code, html) {
-  return api._fetch('/api/settings/error-pages', {
+  return api.request('/api/settings/error-pages', {
     method: 'PUT',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ code, html })
+    body: { code, html }
   });
 }
